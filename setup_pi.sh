91eebd1a48ba3bee955404f5361d50e81c9297f9
Robby Ranshous <rranshous@gmail.com>
2016-10-10 16:32:23 -0400

    update script to look for transparent black file at full path

--- setup_pi.sh
+++ setup_pi.sh
@@ -2,36 +2,8 @@
 
 # vanilla raspian:
 
-if grep -q "overlay" /etc/modules; then
-  echo "overlay already in modules"
-else
-  echo "adding overlay module"
-  echo "overlay" | sudo tee -a /etc/modules
-fi
-
-echo "installing apt-transport-https"
-sudo apt-get install -y apt-transport-https
-
-echo "downloading key"
-wget -q https://packagecloud.io/gpg.key -O - | sudo apt-key add -
-
-echo "adding hypriot source"
-echo 'deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ wheezy main' | sudo tee /etc/apt/sources.list.d/hypriot.list
-
 echo "updating packages"
 sudo apt-get update
-
-echo "installing docker"
-sudo apt-get install -y docker-hypriot
-
-echo "enabling docker"
-sudo systemctl enable docker
-
-echo "adding docker group"
-sudo usermod -a -G docker pi
-
-echo "pulling rsync image"
-sudo docker pull rranshous/rsyncd-rpi
 
 echo "install fbi"
 sudo apt-get install fbi
@@ -57,9 +29,14 @@ git clone http://github.ove.local/rranshous/infoshower.git
 echo "making data dir"
 mkdir -p /home/pi/infoshower-data
 
-echo "turn off screen saver"
-
 echo "set up rsync"
-sudo docker run --restart=always -d --name=infoshower-rsync -v /home/pi/infoshower-data:/data -e RSYNC_PASSWORD=anonymous rranshous/rsyncd-rpi -avzh --delete  anonymous@infoshower.aws-dev.manheim.com::data/ ./
+if grep -q "rsync" /etc/crontab; then
+  echo "Crontab entry exists"
+else
+  echo "adding crontab entry"
+  echo "*/5 *   * * *   root    RSYNC_PASSWORD=anonymous rsync -avzh --delete  anonymous@infoshower.aws-dev.manheim.com::data/ /home/pi/infoshower-data" | sudo tee -a /etc/crontab
+fi
+
+#echo "turn off screen saver"
 
 echo "done"
